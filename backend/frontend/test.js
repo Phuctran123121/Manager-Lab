ab8c2f35a1e58be793938963c0aac85b56aa37a8
phucbro852-cpu <phucbro852@gmail.com>
2026-04-19 10:20:41 +0700

    100%done

--- backend/frontend/test.js
+++
@@ -1,4 +0,0 @@
-const axios = require('axios');
-axios.post('http://localhost:5000/api/auth/login', { username: 'admin', password: 'admin123' })
-  .then(res => console.log('SUCCESS:', res.data))
-  .catch(err => console.log('ERROR:', err.message));
